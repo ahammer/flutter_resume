9c95010caba4361268a6942b467cf2cf483db88a
Adam Hammer <adamhammer2@gmail.com>
2019-08-11 18:37:05 -0700

    Updated experience section

--- lib/experience/experience.dart
+++ lib/experience/experience.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:resume_flutter/details_screen.dart';
 import 'package:resume_flutter/main.dart';
+
+final dateFormat = DateFormat("MMM yyyy");
 
 class ResumeEntry {
   final String company;
@@ -11,21 +14,55 @@ class ResumeEntry {
 
   ResumeEntry(this.company, this.startDate, this.endDate, this.jobTitle,
       this.achievements);
+
+  String lengthString() { 
+    final timeMs = endDate.millisecondsSinceEpoch - startDate.millisecondsSinceEpoch;
+    final timeMonths = ((timeMs / 2629746000) % 12).toInt();
+    final timeYears = ((timeMs / 2629746000) / 12).toInt();
+    
+
+    
+    if (timeYears == 0) {
+      return "$timeMonths Months";
+    } else if (timeMonths == 0) {
+      return "$timeYears Years";
+    }
+    return "$timeYears Years $timeMonths Months";
+  }
 }
 
 final List<ResumeEntry> resume = [
-  ResumeEntry("Realtor.com", DateTime.now(), DateTime.now(), "jobTitle",
-      ["Realtor.com", "did something else"]),
-  ResumeEntry("PNI Digital Media", DateTime.now(), DateTime.now(), "jobTitle",
-      ["PNI Digital Media", "did something else"]),
-  ResumeEntry("Tio Digital Networks", DateTime.now(), DateTime.now(), "jobTitle",
-      ["did something", "did something else"]),
-  ResumeEntry("Atimi Software", DateTime.now(), DateTime.now(), "jobTitle",
-      ["did something", "did something else"]),
-  ResumeEntry("Siberra", DateTime.now(), DateTime.now(), "jobTitle",
-      ["did something", "did something else"]),
-  ResumeEntry("Tio Digital Galleries", DateTime.now(), DateTime.now(), "jobTitle",
-      ["did something", "did something else"])
+  ResumeEntry(
+      "Realtor.com", DateTime(2015, 5), DateTime.now(), "Staff Engineer", [
+    "Created Flutter based framework for Realtor.com IOS/Android apps",
+    "Maintained Realtor.com Android App",
+    "Spearheaded large-scale projects within the team",
+    "Developed Instant-App",
+    "Helped drive a large improvement in customer satisfation"
+  ]),
+  ResumeEntry("Metal Rain Studios", DateTime(2010, 6), DateTime.now(),
+      "Founder/Developer", [
+    "Developed open source CMS (MySaasa)",
+    "Published independent games",
+    "Published dozens of small utilities and app"
+  ]),
+  ResumeEntry("PNI Digital Media", DateTime(2013, 11), DateTime(2014, 10),
+      "Senior Android Developer", [
+    "Reduced bugs and increased stability of production application",
+    "Improved structure and processes of the team",
+    "Introduced CI and Testing framework"
+  ]),
+  ResumeEntry("TIO Networks", DateTime(2011, 11), DateTime(2013, 11),
+      "Senior Android Developer", [
+    "Developed TIO Mobile BillPay",
+    "Deployed branded solutions of the BillPay platform",
+    "Help lead the company towards Agile Development"
+  ]),
+  ResumeEntry("Atimi Software", DateTime(2010, 12), DateTime(2011, 10),
+      "Senior Android Developer", [
+    "Lead Developer of the Mobile Sports Framework",
+    "Deployed apps for the Canucks, Lions and many more NHL and CFL teams"
+  ]),
 ];
 
 class ResumeScreen extends StatelessWidget {
@@ -46,25 +83,32 @@ class ResumeEntryWidget extends StatelessWidget {
 
   const ResumeEntryWidget({Key key, this.entry}) : super(key: key);
 
-  Widget build(BuildContext context) => Card(
-    child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
+  Widget build(BuildContext context) => Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          Row(
             children: <Widget>[
-              Row(
+              Text(entry.company, style: Theme.of(context).textTheme.headline),
+              Spacer(),
+              Text(
+                  "${dateFormat.format(entry.startDate)} - ${dateFormat.format(entry.endDate)}"),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text(entry.jobTitle),
+              Spacer(),
+              Text(entry.lengthString())
+            ],
+          ),
+          Container(height: 8),
+          ...entry.achievements.map((achievement) => Row(
                 children: <Widget>[
-                  Text(entry.company, style: Theme.of(context).textTheme.headline),
-                  Spacer(),
-                  Text("${entry.startDate.toString()} - ${entry.endDate.toString()}"),
-                  
+                  Icon(Icons.arrow_right),
+                  Text(achievement),
                 ],
-              ),
-             Row(
-               children: <Widget>[
-                 Text(entry.jobTitle),
-               ],
-             )],
-          )
-        ),
-  );
+              ))
+        ],
+      ));
 }
