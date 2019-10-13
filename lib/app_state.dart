95efa17f8f90a4d5145f8b3fdf072f221df8f694
Adam Hammer <adamhammer@gmail.com>
2019-10-13 16:56:53 -0700

    putting back light/dark scoped better

---
+++ lib/app_state.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AppState extends ChangeNotifier {
+  String _theme = "Dark";
+  void setTheme(String theme) {
+    _theme = theme;
+    notifyListeners();
+  }
+
+  String getTheme() => _theme;
+}
+
+class AppStateWidget extends StatelessWidget {
+  final Widget child;
+
+  const AppStateWidget({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider<AppState>(
+        builder: (ctx) => AppState(),
+        child: child,
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:resume_flutter/app_state.dart';
 import 'package:resume_flutter/screens/about/about_screen.dart';
 import 'package:resume_flutter/screens/about/resume_screen.dart';
 import 'package:resume_flutter/theme.dart';
 import 'package:resume_flutter/transitions/fade_route.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 const kSections = ["About", "Resume"];
 
 class MyApp extends StatelessWidget {
-  final String currentTheme = "Light";
   final String currentSection = "About";
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Adam Hammer',
-        debugShowCheckedModeBanner: false,        
-        theme: getTheme(currentTheme),
-        home: MyPortfolioScaffold(route: "Resume", theme: "Light", child: ResumeScreen(),),        
-        
-        onGenerateRoute: (settings) {
-          switch(settings.name) {
-            case "/About" : 
-              return FadeRoute(page: MyPortfolioScaffold(route: "About", theme: "Light", child: AboutScreen(),),        );
-            case "/Resume" : 
-              return FadeRoute(page: MyPortfolioScaffold(route: "Resume", theme: "Light", child: ResumeScreen(),),        );
-            default:
-              return FadeRoute(page:Container(child:Text("Empty")));
-          }
-        }, 
-
-        );  
+    return AppStateWidget(
+        child: Consumer<AppState>(
+            builder: (context, appstate, _) => MaterialApp(
+                  title: 'Adam Hammer',
+                  debugShowCheckedModeBanner: false,
+                  theme: getTheme(appstate.getTheme()),
+                  home: MyPortfolioScaffold(state:appstate,
+                    route: "Resume",
+                    theme: appstate.getTheme(),
+                    child: ResumeScreen(),
+                  ),
+                  onGenerateRoute: (settings) {
+                    switch (settings.name) {
+                      case "/About":
+                        return FadeRoute(
+                          page: MyPortfolioScaffold(state:appstate,
+                            route: "About",
+                            theme: appstate.getTheme(),
+                            child: AboutScreen(),
+                          ),
+                        );
+                      case "/Resume":
+                        return FadeRoute(
+                          page: MyPortfolioScaffold(state:appstate,
+                            route: "Resume",
+                            theme: appstate.getTheme(),
+                            child: ResumeScreen(),
+                          ),
+                        );
+                      default:
+                        return FadeRoute(page: Container(child: Text("Empty")));
+                    }
+                  },
+                )));
   }
-
-    
 }
 
 class MyPortfolioScaffold extends StatelessWidget {
+  final AppState state;
   final String route;
   final String theme;
   final Widget child;
 
   const MyPortfolioScaffold({
-    Key key, this.route, this.theme, this.child,
+    Key key,
+    this.route,
+    this.theme,
+    this.child, @required this.state,
   }) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          appBar: AppBar(
-            title: MainScreenNavBar(
-                selected: route,),
-            actions: <Widget>[
-            ],
+        appBar: AppBar(
+          title: MainScreenNavBar(
+            selected: route,
           ),
-          body: (child == null)?ResumeScreen():child);
+          actions: <Widget>[
+            DropdownButton(onChanged: (value)=>state.setTheme(value), value:state.getTheme(),items: ["Light", "Dark"].map((item)=>DropdownMenuItem(value: item, child: Text(item))).toList()),
+          ],
+        ),
+        
+        body: (child == null) ? ResumeScreen() : child);
   }
 }
 
-
 class MainScreenNavBar extends StatelessWidget {
-  
   final String selected;
 
   const MainScreenNavBar({
     Key key,
-    
     @required this.selected,
   }) : super(key: key);
 
@@ -77,16 +95,17 @@ class MainScreenNavBar extends StatelessWidget {
       children: <Widget>[
         ...kSections.map((job) => Container(
               child: RaisedButton(
-                  color: (selected == job)
-                      ? Theme.of(context).chipTheme.selectedColor
-                      : Theme.of(context).chipTheme.disabledColor,
-                  child: Text(
-                    job,
-                    style: Theme.of(context).textTheme.button,
-                  ), onPressed: () {
-                    Navigator.of(context).pushReplacementNamed("/"+job);
-                  },
-                  ),
+                color: (selected == job)
+                    ? Theme.of(context).chipTheme.selectedColor
+                    : Theme.of(context).chipTheme.disabledColor,
+                child: Text(
+                  job,
+                  style: Theme.of(context).textTheme.button,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushReplacementNamed("/" + job);
+                },
+              ),
             )),
       ],
     );
